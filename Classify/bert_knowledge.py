f66cb31affb4ac976b647f6c5c38a169d02fcbf0
RuiqingDing <48399507+RuiqingDing@users.noreply.github.com>
2022-09-02 17:18:31 +0800

    submit code

--- Classify/bert_knowledge.py
+++ Classify/bert_knowledge.py
@@ -8,14 +8,14 @@ from torch.autograd import Variable
 import argparse
 import numpy as np
 import time
-from pytorch_pretrained import BertModel, BertTokenizer
-from pytorch_pretrained.optimization import BertAdam
+from Classify.pytorch_pretrained import BertModel, BertTokenizer
+from Classify.pytorch_pretrained.optimization import BertAdam
 from sklearn import metrics
-from load_data import *
-from aug_quality_assessment import *
+from Classify.load_data import *
+from Classify.aug_quality_assessment import *
 
 parser = argparse.ArgumentParser(description='PyTorch Model Training')
-parser.add_argument('--bert_path', type = str, default = '../PLMs/BERT_base_chinese')
+parser.add_argument('--bert_path', type = str, default = 'BERT_base_chinese')
 parser.add_argument('--dataname', type = str, default = 'CMID')
 parser.add_argument('--datatype', type = str, default = 'Origin')
 parser.add_argument('--num_classes', type = int, default = 4)
@@ -28,13 +28,12 @@ opt = parser.parse_args()
 
 
 class Config(object):
-    """配置参数"""
     def __init__(self):
         self.num_classes = opt.num_classes
         self.class_list = [i for i in range(self.num_classes)]
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
-        self.require_improvement = 500                           # 若超过500 iterations 效果还没提升，则提前结束训练
-        self.num_epochs = opt.epochs                                       # epoch数
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   
+        self.require_improvement = 500                          
+        self.num_epochs = opt.epochs                                      
         self.bert_path = opt.bert_path
         self.tokenizer = BertTokenizer.from_pretrained(self.bert_path)
         self.dataname = opt.dataname
@@ -43,7 +42,6 @@ class Config(object):
         self.lr = opt.lr # learning rate
         self.aug_num = opt.aug_num
         self.delta = opt.delta
-        self.data_path = f'../data/{self.dataname}/{self.datatype}.txt'
 
         
 class BERT(nn.Module):
@@ -68,7 +66,7 @@ def get_time_dif(start_time):
     time_dif = end_time - start_time
     return round(time_dif, 2)
 
-# 训练model
+
 def train(config, model, train_iter, dev_iter, learning_rate, save_file):
     start_time = time.time()
     model.train()
@@ -81,10 +79,10 @@ def train(config, model, train_iter, dev_iter, learning_rate, save_file):
                          lr=learning_rate,
                          warmup=0.05,
                          t_total=len(train_iter) * config.num_epochs)
-    total_batch = 0  # 记录进行到多少batch
+    total_batch = 0  
     dev_best_loss = float('inf')
-    last_improve = 0  # 记录上次验证集loss下降的batch数
-    flag = False  # 记录是否很久没有效果提升
+    last_improve = 0  
+    flag = False  
     model.train()
     for epoch in range(config.num_epochs):
         print('Epoch [{}/{}]'.format(epoch + 1, config.num_epochs))
@@ -95,7 +93,6 @@ def train(config, model, train_iter, dev_iter, learning_rate, save_file):
             loss.backward()
             optimizer.step()
             if total_batch % 50 == 0:
-                # 每多少轮输出在训练集和验证集上的效果
                 true = labels.data.cpu()
                 predic = torch.max(outputs.data, 1)[1].cpu()
                 train_acc = metrics.accuracy_score(true, predic)
@@ -138,11 +135,10 @@ def test(config, model, test_iter, save_file):
     time_dif = get_time_dif(start_time)
     print("Time usage:", time_dif)
 
-    with open(f"result/{config.dataname}_{opt.datatype}_{config.aug_num}_{config.bert_path.split('/')[-1]}_predict_seed_{opt.seed}.txt", "w", encoding="utf-8") as f:
+    with open(f"Classify/result/{config.dataname}_{opt.datatype}_{config.aug_num}_{config.bert_path.split('/')[-1]}_predict_seed_{opt.seed}.txt", "w", encoding="utf-8") as f:
         f.write(str(test_report))
 
 
-# 迭代后计算并打印出当前iteration上训练集的loss/accurancy和测试集的loss/accurancy
 def evaluate(config, model, data_iter, test=False):
     model.eval()
     loss_total = 0
@@ -182,7 +178,7 @@ learning_rate = config.lr
 np.random.seed(opt.seed)
 torch.manual_seed(opt.seed)
 torch.cuda.manual_seed_all(opt.seed)
-torch.backends.cudnn.deterministic = True  # 保证每次结果一样
+torch.backends.cudnn.deterministic = True  
 
 df_filter = filter_text(config.dataname, config.datatype, config.aug_num, config.bert_path, config.num_classes, config.delta)
 print(df_filter.head(5))
@@ -190,17 +186,16 @@ print(df_filter.head(5))
 train_iter, dev_iter, test_iter, train_num, dev_num, test_num = load_filter_dataset(config.dataname, df_filter, config.bert_path, config.aug_num)
 
 # train
-###释放CUDA内存
 if hasattr(torch.cuda, 'empty_cache'):
     torch.cuda.empty_cache()
 model = BERT(config).to(config.device)
     
 time_train_start = time.time()
-train(config, model, train_iter, dev_iter, learning_rate, f"save_dict/{config.dataname}_{config.datatype}_Filter_{config.aug_num}_{config.bert_path.split('/')[-1]}_{opt.seed}.ckpt")
+train(config, model, train_iter, dev_iter, learning_rate, f"Classify/save_dict/{config.dataname}_{config.datatype}_Filter_{config.aug_num}_{config.bert_path.split('/')[-1]}_{opt.seed}.ckpt")
 print('train spending: ', get_time_dif(time_train_start))
 
 time_test_start = time.time()
-test(config, model, test_iter, f"save_dict/{config.dataname}_{config.datatype}_Filter_{config.aug_num}_{config.bert_path.split('/')[-1]}_{opt.seed}.ckpt")
+test(config, model, test_iter, f"Classify/save_dict/{config.dataname}_{config.datatype}_Filter_{config.aug_num}_{config.bert_path.split('/')[-1]}_{opt.seed}.ckpt")
 print('test spending: ', get_time_dif(time_test_start))
 
 time_dif = get_time_dif(start_time)
